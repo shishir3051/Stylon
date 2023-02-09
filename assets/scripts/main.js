98ee2c02ca586d10d09196e941b99f900630a37f
shishir3051 <gaziur35-3051@diu.edu.bde.com>
2023-02-09 19:32:41 +0600

    slider section complete

---
+++ assets/scripts/main.js
@@ -0,0 +1,19 @@
+  <!-- Swiper JS -->
+  <s src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></s
+
+  <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  </script>
