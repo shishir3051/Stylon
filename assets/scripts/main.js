dcdf340cab72d00966312696d5271f20bfb38b51
shishir3051 <gaziur35-3051@diu.edu.bde.com>
2023-02-13 20:08:32 +0600

    all complete

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,8 @@
-  <!-- Swiper JS -->
-  <s src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></s
+var navLinks = document.getElementById("navLinks");
 
-  <!-- Initialize Swiper -->
-  <script>
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-  </script>
+function showMenu() {
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
